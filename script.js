2950432ac5d32b46b7ecf6f791a0e728699c9374
jeyachandran17 <jeyachandran1733@gmail.com>
2022-12-28 17:13:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(()=>{
+    console.log("10");
+    document.querySelector("P").innerHTML="10"
+    setTimeout(()=>{
+        console.log("9");
+        document.querySelector("P").innerHTML="9"
+        setTimeout(()=>{
+            console.log("8");
+            document.querySelector("P").innerHTML="8"
+            setTimeout(()=>{
+                console.log("7");
+                document.querySelector("P").innerHTML="7"
+                setTimeout(()=>{
+                    console.log("6");
+                    document.querySelector("P").innerHTML="6"
+                    setTimeout(()=>{
+                        console.log("5");
+                        document.querySelector("P").innerHTML="5"
+                        setTimeout(()=>{
+                            console.log("4");
+                            document.querySelector("P").innerHTML="4"
+                            setTimeout(()=>{
+                                console.log("3");
+                                document.querySelector("P").innerHTML="3"
+                                setTimeout(()=>{
+                                    console.log("2");
+                                    document.querySelector("P").innerHTML="2"
+                                    setTimeout(()=>{
+                                        console.log("1");
+                                        document.querySelector("P").innerHTML="1"
+                                        setTimeout(()=>{
+                                            console.log("Happy Independence Day");
+                                            document.querySelector("P").innerHTML="Happy Independance Day"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},2000)
